657cfdeb6f486168883466e2842ef832b63f6654
teakyung25 <taekyung.kim25@gmail.com>
2020-09-06 11:52:58 -0600

    resync repo with new .gitignore

--- public/index.js
+++ public/index.js
@@ -1,10 +1,8 @@
 $(function(){
-
     $(".updown").click((e)=>{
         console.log(e.currentTarget.parentElement.childNodes);
         $(e.currentTarget.parentElement.childNodes[5]).toggle();
         console.log($('#item_toggle'));
         ($("#item_toggle").text() == 'keyboard_arrow_up') ? $("#item_toggle",this).text('keyboard_arrow_down') : $("#item_toggle",this).text('keyboard_arrow_up');
     })
-
 })
