a97f988de6dc8aac8fa018e54478e96e838334c4
teakyung25 <taekyung.kim25@gmail.com>
2020-10-14 15:24:03 -0600

    Added Your Ideas page

--- public/index.js
+++ public/index.js
@@ -5,4 +5,11 @@ $(function(){
         console.log($('#item_toggle'));
         ($("#item_toggle").text() == 'keyboard_arrow_up') ? $("#item_toggle",this).text('keyboard_arrow_down') : $("#item_toggle",this).text('keyboard_arrow_up');
     })
+    $(window).scroll(() => {
+        var first = $(window).height();
+        if($(this).scrollTop() >= first) {
+            console.log("hi");
+            $("#user_nav").css("position","absolute");
+        }
+    })
 })
