865c388a36ae15626c31bf33ed3864ad738f1253
StonebayKyle <stonebay.kyle@gmail.com>
2020-10-15 13:33:24 -0600

    Adds button to my ideas page

--- public/index.js
+++ public/index.js
@@ -1,4 +1,5 @@
 $(function(){
+    // feed post
     $("#btnNewPost").click(function() {
         console.log("New Post Button Pressed")
         post = new Post("Title Name", "This is a description!", "./pics/1.jpg");
