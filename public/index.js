a31a05e0f13df877bc6192e30f212892d4b1dded
StonebayKyle <stonebay.kyle@gmail.com>
2020-10-15 14:16:33 -0600

    Adds dynamically adding new ideas (in my ideas section)

--- public/index.js
+++ public/index.js
@@ -24,18 +24,40 @@ $(function(){
         `);
     })
 
+    // my ideas post
+    $("#newIdea").click(function() {
+        console.log("New Post!")
+        post = new Post("Title", "This is a brief description of my idea post", "./pics/3.jpg")
+
+        $("#idea_container").append(`
+            <div class="idea_item" id="idea1">
+                <div class="idea-thumb">
+                    <img src="${post.imageSrc}" alt="">
+                </div>
+                <div class="idea-title">
+                    ${post.name}
+                </div> 
+                <span class="flex_grow"></span>
+                <button class="idea_info destyle_btn" ><i class="material-icons">info</i></button>
+                <button class="idea_message destyle_btn" ><i class="material-icons">message</i></button>
+                <button class="idea_edit destyle_btn" ><i class="material-icons">edit</i></button>
+            </div>
+        `)
+    })
+
     $(document).on("click", ".updown", (e)=>{
         console.log(e.currentTarget.parentElement.childNodes[5]);
         $(e.currentTarget.parentElement.childNodes[5]).toggle();
         console.log(e.currentTarget.parentElement.childNodes[1].children[0].innerText);
         (e.currentTarget.parentElement.childNodes[1].children[0].innerText == 'keyboard_arrow_up') ? $(e.currentTarget.parentElement.childNodes[1].children[0]).text('keyboard_arrow_down') : $(e.currentTarget.parentElement.childNodes[1].children[0]).text('keyboard_arrow_up');
     })
+
     $("#close").click(()=>{
         $(".modals2").hide();
     })
-    $(".idea_message").click(()=>{
-        $(".modals2").show();
 
+    $(document).on("click", ".idea_message", (e)=>{
+        $(".modals2").show();
     })
 })
 
