5394b1040b33a42fca1a27b003f8b2520f449433
teakyung25 <taekyung.kim25@gmail.com>
2020-10-17 00:41:36 -0600

    Message modal works now, appends to site and local storage, still need to work on appending contributer and keywords

--- public/index.js
+++ public/index.js
@@ -35,48 +35,76 @@ $(function(){
     // my ideas post
     $("#newIdea").click(function() {
         $("#idea_create_modal").show();
-        // console.log("New Post!")
-        // post = new Post("Title", "This is a brief description of my idea post", "./pics/3.jpg")
-
-        // $("#idea_container").append(`
-        //      <div class="idea_item" id="idea1">
-        //                 <div class="idea-preview">
-        //                     <div class="idea-thumb">
-        //                         <img src="${post.imageSrc}" alt="">
-        //                     </div>
-        //                     <div class="idea-title">${post.name}</div> 
-        //                     <span class="flex_grow"></span>
-        //                     <!-- <button class="idea_info destyle_btn" ><i class="material-icons">info</i></button> -->
-        //                     <button class="idea_message destyle_btn" ><i class="material-icons">message</i></button>
-        //                 </div>
-        //                 <div class="idea-content noselect">
-        //                     <div class="summary">
-        //                         <h3>${post.name}</h3>
-        //                         <h4>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ultricies pellentesque sem, nec dapibus turpis bibendum sit amet. </h4>
-        //                         <div class="tags">
-        //                             <div class="tag" id="coding">Coding</div>
-        //                             <div class="tag" id="art">Art</div>
-        //                         </div>
-        //                     </div>
-        //                     <div class="info">
-        //                         <div class="info">
-        //                             <i class="material-icons">access_time</i>
-        //                             <h5 id="time_created">10/23/12</h5>
-        //                         </div>
-        //                         <div class="info">
-        //                             <i class="material-icons">person</i>
-        //                             <h5 id="owner">Bob ki</h5>
-        //                         </div>
-        //                         <div class="info">
-        //                             <i class="material-icons">groups</i>
-        //                             <h5 id="contributers">Jams, Bob, gjes</h5>
-        //                         </div>
-        //                     </div>
-        //                 </div>
-        //             </div>
-        // `)
     })
-    
+
+
+    //Create Idea Form
+    $("body").on('submit', "#create_idea_form", (e)=>{
+        e.preventDefault();
+        let data = $("#create_idea_form").serializeArray();
+        let allSelect = document.querySelectorAll(".selected");
+        let keywords = [];
+        let date = new Date();
+        console.log(allSelect[0]);
+        for(var i = 0; i < allSelect.length; i++){
+            console.log(allSelect[i].id);
+            keywords.push(allSelect[i].id);
+        }
+        console.log("New Post!")
+        let post = new Post(data[0].value,data[1].value,`./pics/${Math.floor(Math.random() * 8)}.jpg`,"bob","",keywords,date);
+        appendToLocalStorage(post);
+        
+                $("#idea_container").append(`
+                     <div class="idea_item" id="idea1">
+                                <div class="idea-preview">
+                                    <div class="idea-thumb">
+                                        <img src="${post.imageSrc}" alt="">
+                                    </div>
+                                    <div class="idea-title">${post.name}</div> 
+                                    <span class="flex_grow"></span>
+                                    <!-- <button class="idea_info destyle_btn" ><i class="material-icons">info</i></button> -->
+                                    <button class="idea_message destyle_btn" ><i class="material-icons">message</i></button>
+                                </div>
+                                <div class="idea-content noselect">
+                                    <div class="summary">
+                                        <h3>${post.name}</h3>
+                                        <h4>${post.desc}</h4>
+                                        <div class="tags">
+                                            <div class="tag" id="coding">Coding</div>
+                                            <div class="tag" id="art">Art</div>
+                                        </div>
+                                    </div>
+                                    <div class="info">
+                                        <div class="info">
+                                            <i class="material-icons">access_time</i>
+                                            <h5 id="time_created">${post.creationDate}</h5>
+                                        </div>
+                                        <div class="info">
+                                            <i class="material-icons">person</i>
+                                            <h5 id="owner">${post.projectOwner}</h5>
+                                        </div>
+                                        <div class="info">
+                                            <i class="material-icons">groups</i>
+                                            <h5 id="contributers">Jams, Bob, gjes</h5>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                `)
+        $("#idea_create_modal").hide();
+    })
+    //close create idea modal
+    $(document).on("click", "#close_create_idea", (e)=>{
+        $("#idea_create_modal").hide();
+    })
+
+    $(".select_tag").click((e) =>{
+        console.log(e.target.className.substring(0,10));
+        let element = e.currentTarget;
+        $(e.currentTarget).toggleClass("selected");
+    })
+
+
     // message modal
     $(document).on("click", ".idea_message", (e)=>{
         $("#message_modal").show();
@@ -87,10 +115,7 @@ $(function(){
         $("#message_modal").hide();
     })
 
-    //close create idea modal
-    $(document).on("click", "#close_create_idea", (e)=>{
-        $("#idea_create_modal").hide();
-    })
+
 
     //Collapse event lisenter 
     $(document).on("click", ".idea-preview", (e) => {
@@ -183,6 +208,7 @@ function appendToLocalStorage(post) {
     // add any data that needs to be saved here:
     post.indexStoredAt = existingKey.ideaArray.length; // special value for tracking order of storage
     updateFeedID(post);
+    console.log(post.keywords)
 
     existingKey.ideaArray[post.indexStoredAt] = post; // adds post to end of list
 
